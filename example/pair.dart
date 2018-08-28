3c56e79b2f7ff6873466635266e21fad0cacc326
Harm Aarts <harmaarts@gmail.com>
2018-08-28 12:12:14 +0200

    example: add pair CLI tool

---
+++ example/pair.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+import 'dart:io';
+
+import "package:pointycastle/ecc/api.dart";
+import "package:pointycastle/api.dart";
+import 'package:btcpay_client/btcpay-client.dart';
+import 'package:btcpay_client/key_utils.dart';
+
+void main() async {
+  var keyPair = randomSecp256k1KeyPair();
+  save('/tmp/d', keyPair.privateKey);
+
+  var client = Client("https://test2-btc-ltc.forkbitpay.ninja/", keyPair);
+
+  String url = await client.clientInitiatedPairing();
+  print('Visit this url to complete pairing: $url');
+}
