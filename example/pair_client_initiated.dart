4ae8014e1c12020cdbf3e98c814e721963f7c64c
Dominik Spicher <dominik.spicher@inacta.ch>
2019-10-04 10:13:02 +0200

    Use demo BTCPay instance

--- example/pair_client_initiated.dart
+++ example/pair_client_initiated.dart
@@ -5,9 +5,9 @@ import 'package:btcpay_client/key_utils.dart';
 
 void main() async {
   var keyPair = randomSecp256k1KeyPair();
-  save('/tmp/d', keyPair.privateKey);
+  await save('/tmp/d', keyPair.privateKey);
 
-  var client = Client("https://btcpay-ch-1.feathercoin.ch", keyPair);
+  var client = Client("https://mainnet.demo.btcpayserver.org", keyPair);
 
   String url = await client.clientInitiatedPairing(label());
   print('Visit this url to complete pairing: $url');
